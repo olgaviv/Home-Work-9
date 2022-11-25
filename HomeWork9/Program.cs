c1ee8a5276a3841feb67862b0b150ce629a46190
olga <olgaviv@mail.ru>
2022-11-25 10:09:27 +0700

    first home work

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+/*
+void ShowNumbers(int n)
+{
+    Console.Write(n + " ");
+    if (n> 1) 
+    ShowNumbers (n -1);
+    
+}
+Console.WriteLine("Input a number N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ShowNumbers(n);
+*/
+
+// 
