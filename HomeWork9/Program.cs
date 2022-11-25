41cfc17d8d53e41190d85b32be865e01a7a2c1fc
olga <olgaviv@mail.ru>
2022-11-25 10:12:19 +0700

    second  home work

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -12,4 +12,16 @@ int n = Convert.ToInt32(Console.ReadLine());
 ShowNumbers(n);
 */
 
-// 
+//  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int SumNumbers(int m, int n)
+        {
+            if (m == n) return m;                       // Если M=N
+            else if (m < n) return n + SumNumbers(m, n - 1); // Если M<N
+            else return n + SumNumbers(m, n + 1);            // Если M>N
+        }
+        
+            Console.WriteLine("Input a number M: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input a number N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Результат суммы чисел S = {SumNumbers(m, n)}");
