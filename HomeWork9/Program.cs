6e07be41d598f7471394970dcc76849dbc2d95e2
olga <olgaviv@mail.ru>
2022-11-25 10:14:13 +0700

    third  home work

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -13,6 +13,7 @@ ShowNumbers(n);
 */
 
 //  Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+/*
 int SumNumbers(int m, int n)
         {
             if (m == n) return m;                       // Если M=N
@@ -25,3 +26,22 @@ int SumNumbers(int m, int n)
             Console.WriteLine("Input a number N: ");
             int n = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Результат суммы чисел S = {SumNumbers(m, n)}");
+*/
+
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+int Akkerman (int m, int n)
+{
+  if (m == 0)
+    return n + 1;
+  else
+    if ((m != 0) && (n == 0))
+      return Akkerman(m - 1, 1);
+    else
+      return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+            Console.WriteLine("Input a positive number M: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input a positive number N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Akkerman(m,n));
